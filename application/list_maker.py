e5595ea152047210958201ecef849a016d2c6f6e
shan <yaochloe@hotmail.com>
2017-10-14 13:49:35 +0200

    ipdb removed

--- application/list_maker.py
+++ application/list_maker.py
@@ -1,6 +1,6 @@
 import os, csv
 import pandas as pd
-import ipdb
+
 
 # DATA EXTRACTION
 
@@ -8,7 +8,7 @@ PATH = "/home/jeanbaptiste/train"
 l = [(os.path.basename(path), files) for path, dirs, files in os.walk("/home/jeanbaptiste/train")]
 df = pd.DataFrame(l, columns=['name','images'])
 res = df.set_index('name')['images'].apply(pd.Series).stack()
-ipdb.set_trace()
+
 res = res.reset_index()
 res.columns = ["species_name", "item", "image_name"]
 
