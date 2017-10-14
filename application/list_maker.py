5764110b11317d3e1778537848807ca9faae01c4
shan <yaochloe@hotmail.com>
2017-10-14 16:42:39 +0200

    new label file created

--- application/list_maker.py
+++ application/list_maker.py
@@ -2,10 +2,15 @@ import os, csv
 import pandas as pd
 
 
+#-----------------------FILE 1: image name / kaggle label 
+
 # DATA EXTRACTION
 
 PATH = "/home/jeanbaptiste/train"
-l = [(os.path.basename(path), files) for path, dirs, files in os.walk("/home/jeanbaptiste/train")]
+l = [
+	(os.path.basename(path), files) 
+	for path, dirs, files in os.walk("/home/jeanbaptiste/train")
+]
 df = pd.DataFrame(l, columns=['name','images'])
 res = df.set_index('name')['images'].apply(pd.Series).stack()
 
@@ -14,4 +19,25 @@ res.columns = ["kaggle_label", "item", "image_name"]
 
 # WRITE IN CSV
 
-res.to_csv('image_list.csv', columns=['image_name','kaggle_label'], index=False)
+res.to_csv(
+	'image_list.csv', 
+	columns=['image_name','kaggle_label'],
+	index=False
+)
+
+#---------------------- FILE 2: image name/ clustered label
+
+#  READ TRANSLATION FILE
+
+trans_file = 'label_translation.csv'
+trans_df = pd.read_csv(trans_file)
+
+df=pd.merge(res, trans_df, on='kaggle_label')
+
+# WRITE IN CSV
+
+df.to_csv(
+	'newlabels_image_list.csv',
+	columns=['image_name','final_label'],
+	index=False
+)
